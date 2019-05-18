b15dabad4eb18ab70e3feb44dadb4f40563f0298
mattia <dev@abchip.it>
2019-05-18 16:41:08 +0200

    Save update, eClass management

--- org.abchip.mimo.core.http/src/org/abchip/mimo/core/http/servlet/SaveServlet.java
+++ org.abchip.mimo.core.http/src/org/abchip/mimo/core/http/servlet/SaveServlet.java
@@ -13,12 +13,13 @@ package org.abchip.mimo.core.http.servlet;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.abchip.mimo.EMFFrameAdapter;
 import org.abchip.mimo.context.ContextRoot;
 import org.abchip.mimo.core.http.BaseServlet;
 import org.abchip.mimo.entity.EntityNameable;
@@ -29,33 +30,41 @@ import org.abchip.mimo.entity.ResourceSerializer;
 import org.abchip.mimo.entity.SerializationType;
 import org.abchip.mimo.util.Strings;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class SaveServlet extends BaseServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private ResourceManager resourceManager;
 	@Inject
 	private ContextRoot contextRoot;
 
-
-
 	protected void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		_execute(request, response);
 	}
-	
+
 	private <E extends EntityNameable> void _execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		
+
 		String frame = Strings.qINSTANCE.firstToUpper(request.getParameter("frame"));
 		String json = request.getParameter("json");
 
-		InputStream targetStream = new ByteArrayInputStream(json.getBytes());
-		
 		ResourceSerializer<E> resourceSerializer = resourceManager.getResourceSerializer(contextRoot, frame, SerializationType.JSON);
-		resourceSerializer.load(targetStream);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, Object> map  = objectMapper.readValue(json, new TypeReference<Map<String,Object>>(){});
+		if(!map.containsKey("eClass")) {
+			String eClass = ((EMFFrameAdapter<E>) resourceSerializer.getFrame()).getEClass().getEPackage().getNsURI() + "#//" + frame;
+			map.put("eClass", eClass);
+			json = objectMapper.writeValueAsString(map);
+		}
+
+		resourceSerializer.load(new ByteArrayInputStream(json.getBytes()));
 		E entity = null;
 		entity = resourceSerializer.get();
-		
+
 		EntityWriter<EntityNameable> entityWriter = resourceManager.getEntityWriter(contextRoot, frame, ResourceScope.ROOT);
 
 		entityWriter.save(entity);
