78802bc022812db3f3aa105b92f14e43fe07d737
dev@abchip.it <dev@abchip.it>
2019-06-04 21:23:36 +0200

    Update DeleteServlet

--- org.abchip.mimo.core.http/src/org/abchip/mimo/core/http/servlet/DeleteServlet.java
+++ org.abchip.mimo.core.http/src/org/abchip/mimo/core/http/servlet/DeleteServlet.java
@@ -11,26 +11,21 @@
  */
 package org.abchip.mimo.core.http.servlet;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.Map;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.abchip.mimo.EMFFrameAdapter;
 import org.abchip.mimo.context.ContextRoot;
 import org.abchip.mimo.core.http.BaseServlet;
 import org.abchip.mimo.entity.EntityNameable;
 import org.abchip.mimo.entity.EntityWriter;
+import org.abchip.mimo.entity.Frame;
+import org.abchip.mimo.entity.FrameManager;
 import org.abchip.mimo.entity.ResourceManager;
-import org.abchip.mimo.entity.ResourceSerializer;
-import org.abchip.mimo.entity.SerializationType;
+import org.abchip.mimo.entity.ResourceScope;
 import org.abchip.mimo.util.Strings;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DeleteServlet extends BaseServlet {
 
@@ -40,31 +35,26 @@ public class DeleteServlet extends BaseServlet {
 	private ResourceManager resourceManager;
 	@Inject
 	private ContextRoot contextRoot;
+	@Inject
+	private FrameManager frameManger;
+
 
 	protected void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		_execute(request, response);
 	}
 
 	private <E extends EntityNameable> void _execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String frameName = Strings.qINSTANCE.firstToUpper(request.getParameter("frame"));
+		String name = request.getParameter("name");
+		
+		@SuppressWarnings("unchecked")
+		Frame<E> frame = (Frame<E>) frameManger.getFrameReader(contextRoot).lookup(frameName);
+		if (frame == null) 
+			return;
 
-		String frame = Strings.qINSTANCE.firstToUpper(request.getParameter("frame"));
-		String json = request.getParameter("json");
+		E entity = resourceManager.getEntityReader(contextRoot, frame, ResourceScope.CTX).lookup(name);
 
-		ResourceSerializer<E> resourceSerializer = resourceManager.getResourceSerializer(contextRoot, frame, SerializationType.JSON);
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		Map<String, Object> map  = objectMapper.readValue(json, new TypeReference<Map<String,Object>>(){});
-		if(!map.containsKey("eClass")) {
-			String eClass = ((EMFFrameAdapter<E>) resourceSerializer.getFrame()).getEClass().getEPackage().getNsURI() + "#//" + frame;
-			map.put("eClass", eClass);
-			json = objectMapper.writeValueAsString(map);
-		}
-
-		resourceSerializer.load(new ByteArrayInputStream(json.getBytes()));
-		E entity = null;
-		entity = resourceSerializer.get();
-
-		EntityWriter<EntityNameable> entityWriter = resourceManager.getEntityWriter(contextRoot, frame, "data");
+		EntityWriter<EntityNameable> entityWriter = resourceManager.getEntityWriter(contextRoot, frameName, "data");
 
 		entityWriter.delete(entity);
 	}
