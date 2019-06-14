ac6d30e8143e75b9aaf7dc72da70819286e7b6b1
dev@abchip.it <dev@abchip.it>
2019-06-14 11:08:09 +0200

    Creazioni classi LoginStatus & Logout

---
+++ org.abchip.mimo.core.http/src/org/abchip/mimo/core/http/servlet/LogoutServlet.java
@@ -0,0 +1,36 @@
+/**
+ *  Copyright (c) 2017, 2019 ABChip and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *   Mattia Rocchi - Initial API and implementation
+ */
+package org.abchip.mimo.core.http.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.abchip.mimo.context.ContextProvider;
+import org.abchip.mimo.core.http.BaseServlet;
+import org.abchip.mimo.entity.EntityNameable;
+
+public class LogoutServlet extends BaseServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void execute(ContextProvider contextProvider, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		_execute(contextProvider, request, response);
+	}
+
+	private <E extends EntityNameable> void _execute(ContextProvider contextProvider, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getWriter().write("{}");
+
+		response.flushBuffer();
+	}
+}
