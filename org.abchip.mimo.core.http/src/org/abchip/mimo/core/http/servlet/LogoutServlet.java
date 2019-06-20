daff0783f69b23b7e41e27613e5d0db2b668ebe7
mattia <dev@abchip.it>
2019-06-20 22:26:50 +0200

    Refactoring

--- org.abchip.mimo.core.http/src/org/abchip/mimo/core/http/servlet/LogoutServlet.java
+++ org.abchip.mimo.core.http/src/org/abchip/mimo/core/http/servlet/LogoutServlet.java
@@ -10,28 +10,17 @@ package org.abchip.mimo.core.http.servlet;
 
 import java.io.IOException;
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.abchip.mimo.context.ContextProvider;
-import org.abchip.mimo.context.ContextRoot;
 import org.abchip.mimo.core.http.BaseServlet;
-import org.abchip.mimo.entity.EntityNameable;
 
 public class LogoutServlet extends BaseServlet {
 
-	@Inject
-	private ContextRoot contextRoot;
-
 	private static final long serialVersionUID = 1L;
-	
 
 	protected void execute(ContextProvider contextProvider, HttpServletRequest request, HttpServletResponse response) throws IOException {
-		_execute(contextProvider, request, response);
-	}
-
-	private <E extends EntityNameable> void _execute(ContextProvider contextProvider, HttpServletRequest request, HttpServletResponse response) throws IOException {
 		contextProvider.getContext().close();
 		response.setStatus(HttpServletResponse.SC_ACCEPTED);
 		response.flushBuffer();
