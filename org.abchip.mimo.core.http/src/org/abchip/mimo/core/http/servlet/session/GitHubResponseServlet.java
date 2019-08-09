28bbfde94d3097b4407d4c380e575e92e35e301d
dev@abchip.it <dev@abchip.it>
2019-08-09 19:06:05 +0200

    External Login Update

--- org.abchip.mimo.core.http/src/org/abchip/mimo/core/http/servlet/session/GitHubResponseServlet.java
+++ org.abchip.mimo.core.http/src/org/abchip/mimo/core/http/servlet/session/GitHubResponseServlet.java
@@ -9,6 +9,8 @@
 package org.abchip.mimo.core.http.servlet.session;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -17,17 +19,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.abchip.mimo.context.AuthenticationAnonymous;
+import org.abchip.mimo.context.AuthenticationUserToken;
+import org.abchip.mimo.context.ContextFactory;
+import org.abchip.mimo.context.ContextProvider;
+import org.abchip.mimo.core.http.ContextUtils;
+import org.abchip.mimo.entity.EntityNameable;
 import org.abchip.mimo.entity.EntityProvider;
+import org.abchip.mimo.entity.EntityReader;
 import org.abchip.mimo.entity.ResourceManager;
+import org.abchip.mimo.entity.ResourceScope;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GitHubResponseServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-
 	public static final RequestConfig StandardRequestConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
+    public static final String TokenEndpoint = "https://github.com";
+    public static final String TokenServiceUri = "/login/oauth/access_token";
+
 
 	@Inject
 	private ResourceManager resourceManager;
@@ -53,6 +77,7 @@ public class GitHubResponseServlet extends HttpServlet {
 
 	@Override
 	protected final void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		String state = request.getParameter("state");
 
 		HttpSession session = request.getSession();
@@ -60,7 +85,97 @@ public class GitHubResponseServlet extends HttpServlet {
 
 		if (state != session.getId())
 			session.invalidate();
-		
+
 		String authorizationCode = request.getParameter("code");
+
+		/*
+		 * Enumeration<String> paramNames = request.getParameterNames(); while
+		 * (paramNames.hasMoreElements()) { String paramName = paramNames.nextElement();
+		 * System.out.println(paramName + ": " + request.getParameter(paramName)); }
+		 */
+
+		if (authorizationCode == null || authorizationCode.isEmpty()) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Error! GitHub failed fo get authorization code");
+			return;
+		}
+
+		AuthenticationAnonymous authentication = ContextFactory.eINSTANCE.createAuthenticationAnonymous();
+		ContextProvider contextProvider = getDefaultProvider().login(null, authentication);
+
+		// dovremmo accedere con ProductStore e data
+		String entityName = "OAuth2GitHub";
+		EntityReader<?> oauth2Reader = resourceManager.getEntityReader(contextProvider, entityName, ResourceScope.CONTEXT);
+		EntityNameable oauth2GitHub = oauth2Reader.find(null).next();
+
+		this.getDefaultProvider().logout(contextProvider);
+		contextProvider.getContext().close();
+
+		if (oauth2GitHub == null) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Error! GitHub get OAuth2 configuration error");
+			return;
+		}
+
+		String accessToken = null;
+		String idToken = null;
+
+		HttpPost postMethod = null;
+		try (CloseableHttpClient client = HttpClients.custom().build()) {
+			String clientId = oauth2GitHub.isa().getValue(oauth2GitHub, "clientId").toString();
+			String returnURI = oauth2GitHub.isa().getValue(oauth2GitHub, "returnUrl").toString();
+			String secret = oauth2GitHub.isa().getValue(oauth2GitHub, "clientSecret").toString();
+
+            URI uri = new URIBuilder()
+                    .setScheme(TokenEndpoint.substring(0, TokenEndpoint.indexOf(":")))
+                    .setHost(TokenEndpoint.substring(TokenEndpoint.indexOf(":") + 3))
+                    .setPath(TokenServiceUri)
+                    .setParameter("client_id", clientId)
+                    .setParameter("client_secret", secret)
+                    .setParameter("code", authorizationCode)
+                    .setParameter("redirect_uri", returnURI)
+                    .build();
+			
+			postMethod = new HttpPost(uri);
+			postMethod.setConfig(StandardRequestConfig);
+            postMethod.setHeader("Accept", "application/json");
+
+			HttpResponse postResponse = client.execute(postMethod);
+			String responseString = new BasicResponseHandler().handleResponse(postResponse);
+			if (postResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+				JsonFactory factory = new JsonFactory();
+				JsonParser parser = factory.createParser(responseString);
+				HashMap<?, ?> userMap = new ObjectMapper().readValue(parser, HashMap.class);
+				accessToken = (String) userMap.get("access_token");
+				idToken = (String) userMap.get("token_type");
+				// Debug.logInfo("Generated Access Token : " + accessToken, module);
+			} else {
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Error! GitHub get OAuth2 access token error");
+				return;
+			}
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+			return;
+		}
+
+		// checkLogin
+		AuthenticationUserToken authenticationUserToken = ContextFactory.eINSTANCE.createAuthenticationUserToken();
+		authenticationUserToken.setIdToken(idToken);
+		authenticationUserToken.setAccessToken(accessToken);
+		authenticationUserToken.setProvider("GitHub");
+
+		if (this.getDefaultProvider().checkLogin(authenticationUserToken, true)) {
+			ContextUtils.removeContextProvider(state);
+			contextProvider = this.getDefaultProvider().login(state, authenticationUserToken);
+			ContextUtils.addContextProvider(contextProvider);
+
+			String location = response.encodeURL("http://localhost:8081");
+			// System.err.println(("Response location: " + location));
+
+			response.sendRedirect(location);
+		} else {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Error in google response");
+			return;
+		}
+
+
 	}
 }
