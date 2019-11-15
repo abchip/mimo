f264d521c7e5958729afebfd8878a4e848403d54
dev@abchip.it <dev@abchip.it>
2019-11-15 16:32:16 +0100

    ExportPartyVcard update

---
+++ org.abchip.mimo.biz.base/src/org/abchip/mimo/biz/base/service/PartyServices.java
@@ -0,0 +1,144 @@
+package org.abchip.mimo.biz.base.service;
+
+import org.abchip.mimo.biz.party.contact.PostalAddress;
+import org.abchip.mimo.biz.party.contact.TelecomNumber;
+import org.abchip.mimo.biz.party.party.Party;
+import org.abchip.mimo.biz.party.party.PartyGroup;
+import org.abchip.mimo.biz.party.party.Person;
+import org.abchip.mimo.context.ContextProvider;
+import org.abchip.mimo.resource.ResourceManager;
+import org.abchip.mimo.resource.ResourceReader;
+
+import ezvcard.VCard;
+import ezvcard.parameter.AddressType;
+import ezvcard.parameter.TelephoneType;
+import ezvcard.property.Address;
+import ezvcard.property.Email;
+import ezvcard.property.StructuredName;
+import ezvcard.property.Telephone;
+
+public class PartyServices {
+
+	public static ezvcard.VCard createVcardFromParty(ResourceManager resourceManager, ContextProvider contextProvider,
+			String partyId) {
+		ResourceReader<Party> partyReader = resourceManager.getResourceReader(contextProvider, Party.class);
+		ResourceReader<Person> personReader = resourceManager.getResourceReader(contextProvider, Person.class);
+		ResourceReader<PartyGroup> partyGroupReader = resourceManager.getResourceReader(contextProvider,
+				PartyGroup.class);
+		Party partyEntity = partyReader.lookup(partyId);
+
+		ezvcard.VCard vcard = new ezvcard.VCard();
+		StructuredName structuredName = new StructuredName();
+		if (partyEntity == null) {
+			// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			// return;
+
+			// mock
+			structuredName.setGiven("Nome");
+			structuredName.setFamily("Cognome");
+//			fullName = "MockVcard";
+			vcard.setStructuredName(structuredName);
+			Address address = new Address();
+			address.setStreetAddress("via Manzoni n.1");
+			address.setLocality("Milano");
+			address.setPostalCode("20100");
+			// StateProvinceGeo per decodifica
+			address.setRegion("Milano");
+			// CountryGeo per decodifica
+			address.setCountry("Italia");
+			address.getTypes().add(AddressType.WORK);
+			vcard.addAddress(address);
+			Telephone tel = new Telephone("0393381234567");
+			tel.getTypes().add(TelephoneType.WORK);
+			vcard.addTelephoneNumber(tel);
+			vcard.addEmail(new Email("nome.cognome@gmail.com"));
+		} else {
+			// Party or PartyGroup String fullName = "";
+			switch (partyEntity.getPartyTypeId().getPartyTypeId()) {
+			case "PERSON":
+				Person personEntity = personReader.lookup(partyId);
+				if (personEntity.getFirstName() != null && !personEntity.getFirstName().isEmpty()) {
+					structuredName.setGiven(personEntity.getFirstName());
+//					fullName = personEntity.getFirstName();
+				}
+				if (personEntity.getMiddleName() != null && !personEntity.getMiddleName().isEmpty()) {
+					structuredName.setGiven(personEntity.getMiddleName());
+//					fullName += personEntity.getMiddleName();
+				}
+				if (personEntity.getLastName() != null && !personEntity.getLastName().isEmpty()) {
+					structuredName.setFamily(personEntity.getLastName());
+//					fullName += personEntity.getLastName();
+				}
+				break;
+			case "PARTY_GROUP":
+				PartyGroup partyGroupEntity = partyGroupReader.lookup(partyId);
+				if (partyGroupEntity.getGroupName() != null && !partyGroupEntity.getGroupName().isEmpty()) {
+					structuredName.setGiven(partyGroupEntity.getGroupName());
+//					fullName = partyGroupEntity.getGroupName();
+				}
+				break;
+			}
+
+			vcard.setStructuredName(structuredName);
+
+			// PostalAddress
+			writeLatestPostalAddress(resourceManager, contextProvider, vcard, partyId);
+
+			// TelecomNumber
+			writeLatestTelecomNumber(resourceManager, contextProvider, vcard, partyId);
+
+			// E-mail
+			writeLatestEmail(resourceManager, contextProvider, vcard, partyId);
+		}
+
+		return vcard;
+	}
+
+	private static void writeLatestPostalAddress(ResourceManager resourceManager, ContextProvider contextProvider, ezvcard.VCard vcard, String partyId) {
+		PostalAddress postalAddress = ContactMechServices.getLatestPostaAddress(resourceManager, contextProvider,
+				partyId);
+		if (postalAddress == null)
+			return;
+
+		Address address = new Address();
+		if (postalAddress.getAddress1() != null && !postalAddress.getAddress1().isEmpty())
+			address.setStreetAddress(postalAddress.getAddress1());
+		if (postalAddress.getCity() != null && !postalAddress.getCity().isEmpty())
+			address.setLocality(postalAddress.getCity());
+		if (postalAddress.getPostalCode() != null && !postalAddress.getPostalCode().isEmpty())
+			address.setPostalCode(postalAddress.getPostalCode());
+		// StateProvinceGeo per decodifica
+		if (postalAddress.getStateProvinceGeoId() != null)
+			address.setRegion(postalAddress.getStateProvinceGeoId().getGeoId());
+		// CountryGeo per decodifica
+		if (postalAddress.getCountryGeoId() != null)
+			address.setCountry(postalAddress.getCountryGeoId().getGeoId());
+
+		address.getTypes().add(AddressType.WORK);
+		vcard.addAddress(address);
+	}
+
+	private static void writeLatestTelecomNumber(ResourceManager resourceManager, ContextProvider contextProvider, VCard vcard, String partyId) {
+		TelecomNumber telecomNumber = ContactMechServices.getLatestTelecomNumber(resourceManager, contextProvider,
+				partyId);
+		if (telecomNumber == null)
+			return;
+
+		String areaCode = "";
+		String number = "";
+		if (telecomNumber.getAreaCode() != null && !telecomNumber.getAreaCode().isEmpty())
+			areaCode = telecomNumber.getAreaCode();
+		if (telecomNumber.getContactNumber() != null && !telecomNumber.getContactNumber().isEmpty())
+			number = telecomNumber.getContactNumber();
+		Telephone tel = new Telephone(areaCode + number);
+		tel.getTypes().add(TelephoneType.WORK);
+		vcard.addTelephoneNumber(tel);
+	}
+
+	private static void writeLatestEmail(ResourceManager resourceManager, ContextProvider contextProvider, ezvcard.VCard vcard, String partyId) {
+		String email = ContactMechServices.getLatestEmail(resourceManager, contextProvider, partyId);
+		if (email.isEmpty())
+			return;
+		vcard.addEmail(new Email(email));
+	}
+}
--- org.abchip.mimo.biz.base/src/org/abchip/mimo/biz/base/servlet/ExportPartyVcardServlet.java
+++ org.abchip.mimo.biz.base/src/org/abchip/mimo/biz/base/servlet/ExportPartyVcardServlet.java
@@ -16,29 +16,16 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.abchip.mimo.biz.party.contact.ContactMech;
-import org.abchip.mimo.biz.party.contact.PartyContactMech;
-import org.abchip.mimo.biz.party.contact.PostalAddress;
-import org.abchip.mimo.biz.party.contact.TelecomNumber;
-import org.abchip.mimo.biz.party.party.Party;
-import org.abchip.mimo.biz.party.party.PartyGroup;
-import org.abchip.mimo.biz.party.party.Person;
+import org.abchip.mimo.biz.base.service.PartyServices;
 import org.abchip.mimo.context.ContextProvider;
 import org.abchip.mimo.core.http.servlet.BaseServlet;
 import org.abchip.mimo.entity.EntityNameable;
 import org.abchip.mimo.resource.ResourceManager;
-import org.abchip.mimo.resource.ResourceReader;
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.util.IOUtils;
 
 import ezvcard.Ezvcard;
 import ezvcard.VCard;
-import ezvcard.parameter.AddressType;
-import ezvcard.parameter.TelephoneType;
-import ezvcard.property.Address;
-import ezvcard.property.Email;
-import ezvcard.property.StructuredName;
-import ezvcard.property.Telephone;
 
 public class ExportPartyVcardServlet extends BaseServlet {
 
@@ -52,82 +39,11 @@ public class ExportPartyVcardServlet extends BaseServlet {
 	}
 
 	private <E extends EntityNameable> void _execute(ContextProvider contextProvider, HttpServletRequest request, HttpServletResponse response) throws IOException {
-
 		String partyId = request.getParameter("partyId");
-
-		ResourceReader<Party> partyReader = resourceManager.getResourceReader(contextProvider, Party.class);
-		ResourceReader<Person> personReader = resourceManager.getResourceReader(contextProvider, Person.class);
-		ResourceReader<PartyGroup> partyGroupReader = resourceManager.getResourceReader(contextProvider, PartyGroup.class);
-		Party partyEntity = partyReader.lookup(partyId);
-
-		ezvcard.VCard vcard = new ezvcard.VCard();
-		StructuredName structuredName = new StructuredName();
-		String fullName = "";
-
-		if (partyEntity == null) {
-			// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			// return;
-
-			// mock
-
-			structuredName.setGiven("Nome");
-			structuredName.setFamily("Cognome");
-			fullName = "MockVcard";
-			vcard.setStructuredName(structuredName);
-			Address address = new Address();
-			address.setStreetAddress("via Manzoni n.1");
-			address.setLocality("Milano");
-			address.setPostalCode("20100");
-			// StateProvinceGeo per decodifica
-			address.setRegion("Milano");
-			// CountryGeo per decodifica
-			address.setCountry("Italia");
-			address.getTypes().add(AddressType.WORK);
-			vcard.addAddress(address);
-			Telephone tel = new Telephone("0393381234567");
-			tel.getTypes().add(TelephoneType.WORK);
-			vcard.addTelephoneNumber(tel);
-			vcard.addEmail(new Email("nome.cognome@gmail.com"));
-		} else {
-			// Party or PartyGroup String fullName = "";
-			switch (partyEntity.getPartyTypeId().getPartyTypeId()) {
-			case "PERSON":
-				Person personEntity = personReader.lookup(partyId);
-				if (!personEntity.getFirstName().isEmpty()) {
-					structuredName.setGiven(personEntity.getFirstName());
-					fullName = personEntity.getFirstName();
-				}
-				if (!personEntity.getMiddleName().isEmpty()) {
-					fullName += personEntity.getMiddleName();
-				}
-				if (!personEntity.getLastName().isEmpty()) {
-					structuredName.setGiven(personEntity.getLastName());
-					fullName += personEntity.getLastName();
-				}
-				break;
-			case "PARTY_GROUP":
-				PartyGroup partyGroupEntity = partyGroupReader.lookup(partyId);
-				if (!partyGroupEntity.getGroupName().isEmpty()) {
-					structuredName.setGiven(partyGroupEntity.getGroupName());
-					fullName = partyGroupEntity.getGroupName();
-				}
-				break;
-			}
-
-			vcard.setStructuredName(structuredName);
-
-			// PostalAddress
-			writeLatestPostalAddress(contextProvider, vcard, partyId);
-
-			// TelecomNumber
-			writeLatestTelecomNumber(contextProvider, vcard, partyId);
-
-			// E-mail
-			writeLatestEmail(contextProvider, vcard, partyId);
-		}
+		VCard vcard = PartyServices.createVcardFromParty(resourceManager, contextProvider, partyId);
 
 		//
-		File file = new File(fullName + ".vcf");
+		File file = new File(partyId + ".vcf");
 		Ezvcard.write(vcard).go(file);
 
 		// Set the content type and attachment header.
@@ -140,82 +56,6 @@ public class ExportPartyVcardServlet extends BaseServlet {
 			response.flushBuffer();
 		} catch (Exception e) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-		}
-	}
-
-	private void writeLatestPostalAddress(ContextProvider contextProvider, ezvcard.VCard vcard, String partyId) {
-		// Serve l'ordinamento per fromDate desc
-		// e capire come fare il filtro con l'oggetto
-		String filter = "partyId = \"" + partyId + "\"  AND thruDate IS NULL";
-		ResourceReader<PartyContactMech> partyContactMechReader = resourceManager.getResourceReader(contextProvider, PartyContactMech.class);
-		ResourceReader<PostalAddress> postalAddressReader = resourceManager.getResourceReader(contextProvider, PostalAddress.class);
-
-		for (PartyContactMech partyContactMech : partyContactMechReader.find(filter)) {
-			ResourceReader<ContactMech> contactMechReader = resourceManager.getResourceReader(contextProvider, ContactMech.class);
-			ContactMech contactMech = contactMechReader.lookup(partyContactMech.getContactMechId().getContactMechId());
-
-			if (!contactMech.getContactMechTypeId().getContactMechTypeId().equals("POSTAL_ADDRESS"))
-				continue;
-
-			PostalAddress postalAddress = postalAddressReader.lookup(contactMech.getContactMechId());
-			if (postalAddress == null)
-				break;
-
-			Address address = new Address();
-			address.setStreetAddress(postalAddress.getAddress1());
-			address.setLocality(postalAddress.getCity());
-			address.setPostalCode(postalAddress.getPostalCode());
-			// StateProvinceGeo per decodifica
-			address.setRegion(postalAddress.getStateProvinceGeoId().getGeoId());
-			// CountryGeo per decodifica
-			address.setCountry(postalAddress.getCountryGeoId().getGeoId());
-			address.getTypes().add(AddressType.WORK);
-			vcard.addAddress(address);
-			break;
-		}
-	}
-
-	private void writeLatestTelecomNumber(ContextProvider contextProvider, VCard vcard, String partyId) {
-		// Serve l'ordinamento per fromDate desc
-		// e capire come fare il filtro con l'oggetto
-		String filter = "partyId = \"" + partyId + "\"  AND thruDate IS NULL";
-		ResourceReader<PartyContactMech> partyContactMechReader = resourceManager.getResourceReader(contextProvider, PartyContactMech.class);
-		ResourceReader<TelecomNumber> telecomNumberReader = resourceManager.getResourceReader(contextProvider, TelecomNumber.class);
-
-		for (PartyContactMech partyContactMech : partyContactMechReader.find(filter)) {
-			ResourceReader<ContactMech> contactMechReader = resourceManager.getResourceReader(contextProvider, ContactMech.class);
-			ContactMech contactMech = contactMechReader.lookup(partyContactMech.getContactMechId().getContactMechId());
-
-			if (!contactMech.getContactMechTypeId().getContactMechTypeId().equals("TELECOM_NUMBER"))
-				continue;
-
-			TelecomNumber telecomNumber = telecomNumberReader.lookup(contactMech.getContactMechId());
-			if (telecomNumber == null)
-				break;
-
-			Telephone tel = new Telephone(telecomNumber.getAreaCode() + telecomNumber.getContactNumber());
-			tel.getTypes().add(TelephoneType.WORK);
-			vcard.addTelephoneNumber(tel);
-			break;
-		}
-
-	}
-
-	private void writeLatestEmail(ContextProvider contextProvider, ezvcard.VCard vcard, String partyId) {
-		// Serve l'ordinamento per fromDate desc
-		// e capire come fare il filtro con l'oggetto
-		String filter = "partyId = \"" + partyId + "\"  AND thruDate IS NULL";
-		ResourceReader<PartyContactMech> partyContactMechReader = resourceManager.getResourceReader(contextProvider, PartyContactMech.class);
-
-		for (PartyContactMech partyContactMech : partyContactMechReader.find(filter)) {
-			ResourceReader<ContactMech> contactMechReader = resourceManager.getResourceReader(contextProvider, ContactMech.class);
-			ContactMech contactMech = contactMechReader.lookup(partyContactMech.getContactMechId().getContactMechId());
-
-			if (!contactMech.getContactMechTypeId().getContactMechTypeId().equals("EMAIL_ADDRESS"))
-				continue;
-
-			vcard.addEmail(new Email(contactMech.getInfoString()));
-			break;
 		}
 	}
 }
